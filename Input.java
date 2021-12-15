c09f13c16dcc3f4e33c3c861f9ceffa1358672e7
BtlReq <68523680+Kyrin1015@users.noreply.github.com>
2021-12-15 16:27:36 +0000

    Finished the input system(It can detect invalid inputs, requires the user to add proper values), stores the values within 3 lists(amount, reason and tax multiplier). Might add more or change some things, depending on what the client wants

--- Input.java
+++ Input.java
@@ -6,37 +6,86 @@ public class Input {
   public String tempval;
   public ArrayList<String> amount;
   public ArrayList<String> reason;
+  public ArrayList<String> tax;
 
   //constructor
   public Input() {
     run = true;
     amount = new ArrayList<String>();
     reason = new ArrayList<String>();
+    tax = new ArrayList<String>();
     tempval = "";
   }
 
   public void inputLoop() {
     //Rules for user input
-    System.out.println("type 'Exit' to exit the program");
+    System.out.println("type 'Exit' to exit the program, at any point.");
     System.out.println("For reasons, please type in any of the following. Any other inputs will not be accepted.");
     System.out.println("Rent, Cleaning supplies, Salary, Electricity bill, Water bill.");
 
     Scanner keyboard = new Scanner(System.in);
     //user input loop
     while (run == true) {
-      System.out.print("Enter an amount(Before tax):");
+      System.out.print("Enter an amount(Before tax): $");
       tempval = keyboard.nextLine();
-      if (tempval == "Exit") {
-        break;
+      user_exit(tempval);
+      
+      //So the "Exit" typing wont trigger the try-catch
+      if (run == true) {
+        //Try-catch will make sure it is an integer
+        try{
+          Double.parseDouble(tempval);
+        } catch(Exception e) {
+          System.out.println("You entered something other than a number. Try again!");
+          continue;
         }
-      amount.add(tempval);
-      //Double.parseDouble(str)
-      System.out.print("Enter a reason");
-      tempval = keyboard.nextLine();
-      if (tempval == "Exit") {
-        break;
-        }
-      reason.add(tempval);
+        amount.add(tempval);
+
+        System.out.print("Enter a reason: ");
+        tempval = keyboard.nextLine();
+        checkValidReason(tempval);
+        reason.add(tempval);
+      }
+    }
+  }
+  
+
+  //A function that checks whether the reason given is a good parameter, and classifies which taxes(multipliers the amount will face)
+  public void checkValidReason(String tempval) {
+    boolean ongoing = true;
+    while (ongoing == true) {
+      if (tempval.equals("Rent")) {
+        tax.add("1");
+        ongoing = false;
+      } else if (tempval.equals("Cleaning supplies")) {
+        tax.add("1");
+        ongoing = false;
+      } else if (tempval.equals("Salary")) {
+        tax.add("1");
+        ongoing = false;
+      } else if (tempval.equals("Electricity bill")) {
+        tax.add("1");
+        ongoing = false;
+      } else if (tempval.equals("Water bill")) {
+        tax.add("1");
+        ongoing = false;
+      } else if (tempval.equals("Exit")) {
+        run = false;
+        ongoing = false;
+      } else {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Your reason was invalid. Please try again.");
+        System.out.print("Enter a reason: ");
+        tempval = keyboard.nextLine();
+      }
+    }
+  }
+  
+
+  //If user wants to exit
+  public void user_exit(String tempval) {
+    if (tempval.equals("Exit")) {
+      run = false;
     }
   }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 class Main {
 
   public static void main(String[] args) {
-    Output out = new Output();
-    out.total_without_tax();
+    //Output out = new Output();
+    //out.total_without_tax();
+    Input userin = new Input();
+    userin.inputLoop();
   }
 }
