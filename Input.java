9960db18537e804f1939e02255828a53d07ab6c8
BtlReq <68523680+Kyrin1015@users.noreply.github.com>
2022-01-27 05:05:00 +0000

    Added date, seems to be working fine, also added some GUI elements for integration.

--- Input.java
+++ Input.java
@@ -1,16 +1,21 @@
-import java.util.*;
+ import java.util.*;
 import java.io.*;
-import java.time.LocalDate;
 
 public class Input {
   //Class variables
   public boolean run;
   public String tempval;
-  public String date;
   public int size;
   public ArrayList<String> amount;
   public ArrayList<String> reason;
-  public ArrayList<String> tax;
+  public ArrayList<Double> tax;
+  public ArrayList<String> year;
+  public ArrayList<String> month;
+  public ArrayList<String> date;
+
+  public boolean yeartrue;
+  public boolean monthtrue;
+  public boolean datetrue;
 
   //constructor
   public Input() {
@@ -18,9 +23,16 @@ public class Input {
     run = true;
     amount = new ArrayList<String>();
     reason = new ArrayList<String>();
-    tax = new ArrayList<String>();
+    tax = new ArrayList<Double>();
     tempval = "";
-    date = "";
+
+    year = new ArrayList<String>();
+    month = new ArrayList<String>();
+    date = new ArrayList<String>();
+
+    yeartrue = true;
+    monthtrue = true;
+    datetrue = true;
   }
 
   public void inputLoop() {
@@ -31,13 +43,13 @@ public class Input {
 
     Scanner keyboard = new Scanner(System.in);
     //user input loop
-    while (run == true) {
+    while (run) {
       System.out.print("Enter an amount(Before tax): $");
       tempval = keyboard.nextLine();
       user_exit(tempval);
       
       //So the "Exit" typing wont trigger the try-catch
-      if (run == true) {
+      if (run) {
         //Try-catch will make sure it is an integer
         try{
           Double.parseDouble(tempval);
@@ -52,67 +64,59 @@ public class Input {
         checkValidReason(tempval);
         reason.add(tempval);
 
-        try {
-          //Saves dates
-          System.out.println("Enter the year of transaction(Ex. 2019): ");
-          int temp = Integer.parseInt(keyboard.nextLine());
-          date += temp + "/";
-          System.out.println("Enter the month of transaction(1-12): ");
-          temp = Integer.parseInt(keyboard.nextLine());
-          if (temp < 1 || temp > 12) {
-            System.out.println("Month out of range. Please try again!");
-            continue;
-          } else {
-            date += temp + "/";
-            System.out.println("Enter the date of the transaction");
-            temp = Integer.parseInt(keyboard.nextLine());
-            int maxday = 0;
-            if (temp == 1 || temp == 3 || temp == 5 || temp == 7 || temp == 8 || temp == 10 || temp == 12) {
-              maxday = 31;
-            } else if (temp == 2) {
-              maxday = 29;
-            } else {
-              maxday = 30;
-            }
-            if (temp <)
-          }
-        } catch (Exception e){
-          System.out.println("Not a number.Please try again.");
-        }
+        
+        System.out.println("Enter the year, month, then date in numbers(Ex. Month:6).");
+        enterYear();
+        enterMonth();
+        enterDate();
       }
     }
     amountToTxt();
     reasonToTxt();
+    taxToTxt();
+    dateToTxt();
   }
   
 
   //A function that checks whether the reason given is a good parameter, and classifies which taxes(multipliers the amount will face)
   public void checkValidReason(String tempval) {
     boolean ongoing = true;
-    while (ongoing == true) {
+    while (ongoing) {
       if (tempval.equals("Rent")) {
-        tax.add("1");
+        tax.add(1.13);
         ongoing = false;
       } else if (tempval.equals("Cleaning supplies")) {
-        tax.add("1");
+        tax.add(1.13);
         ongoing = false;
       } else if (tempval.equals("Salary")) {
-        tax.add("1");
+        tax.add(1.13);
         ongoing = false;
       } else if (tempval.equals("Electricity bill")) {
-        tax.add("1");
+        tax.add(1.13);
         ongoing = false;
       } else if (tempval.equals("Water bill")) {
-        tax.add("1");
+        tax.add(1.13);
         ongoing = false;
-      } else if (tempval.equals("Exit")) {
+      } 
+      else if (tempval.equals("Sales")) {
+        tax.add(1.13);
+        ongoing = false;
+      }
+      else if (tempval.equals("Exit")) {
         run = false;
         ongoing = false;
-      } else {
+      } 
+      else if (tempval.equals("exit")) {
+        run=false;
+        ongoing=false;
+      } 
+      
+      else {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Your reason was invalid. Please try again.");
         System.out.print("Enter a reason: ");
         tempval = keyboard.nextLine();
+        
       }
     }
   }
@@ -120,8 +124,80 @@ public class Input {
 
   //If user wants to exit
   public void user_exit(String tempval) {
-    if (tempval.equals("Exit")) {
+    if (tempval.equals("Exit")||tempval.equals("exit")) {
       run = false;
+      yeartrue = false;
+      monthtrue = false;
+      datetrue = false;
+    }
+  }
+
+  public void enterYear(){
+    yeartrue = true;
+    while(yeartrue) {
+      Scanner keyboard = new Scanner(System.in);
+      System.out.print("Year: ");
+      tempval = keyboard.nextLine();
+      user_exit(tempval);
+      if (yeartrue) {
+        try{
+          int date = Integer.parseInt(tempval);
+          if (date < 1 ) {
+            System.out.println("Not within scope, please try again.");
+            continue;
+          }
+        } catch(Exception e) {
+          System.out.println("You entered something other than a number. Try again!");
+        }
+        year.add(tempval);
+        yeartrue = false;
+      }
+    }
+  }
+
+  public void enterMonth(){
+    monthtrue = true;
+    while(monthtrue) {
+      Scanner keyboard = new Scanner(System.in);
+      System.out.print("Month: ");
+      tempval = keyboard.nextLine();
+      user_exit(tempval);
+      if (monthtrue) {
+        try{
+          int date = Integer.parseInt(tempval);
+          if (date < 1 || date > 12) {
+            System.out.println("Not within scope, please try again.");
+            continue;
+          }
+        } catch(Exception e) {
+          System.out.println("You entered something other than a number. Try again!");
+        }
+        month.add(tempval);
+        monthtrue = false;
+      }
+    }
+  }
+
+  public void enterDate(){
+    datetrue = true;
+    while(datetrue) {
+      Scanner keyboard = new Scanner(System.in);
+      System.out.print("Date: ");
+      tempval = keyboard.nextLine();
+      user_exit(tempval);
+      if (datetrue) {
+        try{
+          int date = Integer.parseInt(tempval);
+          if (date < 1 || date > 31) {
+            System.out.println("Not within scope, please try again.");
+            continue;
+          }
+        } catch(Exception e) {
+          System.out.println("You entered something other than a number. Try again!");
+        }
+        date.add(tempval);
+        datetrue = false;
+      }
     }
   }
 
@@ -159,6 +235,50 @@ public class Input {
       pw = new PrintWriter(bw);
       for (int i = 0; i < size; i++) {
         pw.println(reason.get(i));
+      }
+      pw.flush();
+      pw.close(); 
+      bw.close(); 
+      fw.close(); 
+    } catch (IOException io) {
+    }
+  }
+
+  public void taxToTxt(){
+    size = tax.size();
+    FileWriter fw = null; 
+    BufferedWriter bw = null; 
+    PrintWriter pw = null;
+    try {
+      File file = new File("Tax.txt");
+      fw = new FileWriter(file, true); 
+      bw = new BufferedWriter(fw); 
+      pw = new PrintWriter(bw);
+      for (int i = 0; i < size; i++) {
+        pw.println(tax.get(i));
+      }
+      pw.flush();
+      pw.close(); 
+      bw.close(); 
+      fw.close(); 
+    } catch (IOException io) {
+    }
+  }
+
+  public void dateToTxt(){
+    String totaldate;
+    size = year.size();
+    FileWriter fw = null; 
+    BufferedWriter bw = null; 
+    PrintWriter pw = null;
+    try {
+      File file = new File("Date.txt");
+      fw = new FileWriter(file, true); 
+      bw = new BufferedWriter(fw); 
+      pw = new PrintWriter(bw);
+      for (int i = 0; i < size; i++) {
+        totaldate = year.get(i) + "-" + month.get(i) + "-" + date.get(i);
+        pw.println(totaldate);
       }
       pw.flush();
       pw.close(); 
--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner; 
+import java.util.ArrayList;
+import java.io.*;
+
+
 class Main {
 
   public static void main(String[] args) {
@@ -5,5 +12,128 @@ class Main {
     //out.total_without_tax();
     Input userin = new Input();
     userin.inputLoop();
-  }
+
+
+//Hari code
+        final JFrame frame = new JFrame("Net Income/Loss");
+int h=4;
+        String[] columns = {"Amount", "Reason", "Tax Amount", "Amount with tax",
+                            "Date"};
+
+
+
+
+       
+       ArrayList<String> cars= new ArrayList<String>();
+         
+           try {
+
+
+
+      FileReader fr = new FileReader("Amount.txt");
+
+      BufferedReader br = new BufferedReader(fr);
+
+      String line = br.readLine();
+      cars.add(line);
+
+      while (line != null) {
+
+        System.out.println(line);
+
+        line = br.readLine();
+        cars.add(line);
+      }
+
+      br.close();
+      fr.close();
+    } catch (Exception ex) {
+
+      System.out.println(ex.getMessage());
+    }
+       
+       int g=cars.size();
+       Object[][] dataa=new Object[g][5];
+int a=0;
+
+
+for(int i=0;i<g;i++){
+
+
+    
+dataa[i][0]=cars.get(a);
+a+=1;
+
+}
+       
+       
+ArrayList<String> Reason= new ArrayList<String>();
+
+try{
+
+      FileReader frr = new FileReader("reason.txt");
+
+      BufferedReader brr = new BufferedReader(frr);
+
+      String lines = brr.readLine();
+      Reason.add(lines);
+
+  while (lines != null) {
+
+        System.out.println(lines);
+
+        lines = brr.readLine();
+        Reason.add(lines);
+      }
+
+      brr.close();
+      frr.close();
+}
+catch(Exception ex){
+System.out.println(ex.getMessage());
+}
+
+int m=Reason.size();
+a=0;
+
+for(int n=0;n<g;n++){
+
+    
+dataa[n][1]=Reason.get(a);
+a+=1;
+
+}
+
+
+        JTable table = new JTable(dataa, columns);
+        JScrollPane scrollPane = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+        
+Scanner sc = new Scanner(System.in);
+        int ans=0;
+        System.out.println("Enter amount:");
+        ans=sc.nextInt();
+
+        String net="";
+        if (ans>0){
+          net="income";
+        }
+        else{
+          net="loss";
+        }
+        JLabel lblHeading = new JLabel("Net "+net);
+        lblHeading.setFont(new Font("Arial",Font.TRUETYPE_FONT,24));
+
+        frame.getContentPane().setLayout(new BorderLayout());
+
+        frame.getContentPane().add(lblHeading,BorderLayout.PAGE_START);
+        frame.getContentPane().add(scrollPane,BorderLayout.CENTER);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(550, 200);
+        frame.setVisible(true);
+        
+        
+    }
+    
 }
